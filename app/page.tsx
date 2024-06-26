277a44500d1aa109075e1cf13a2c801f8acefd98
nipunac99 <nipunachamod99723@gmail.com>
2024-06-27 00:14:36 +0530

    hero-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>This is my Portfolio</h1>
+       <Hero />
       </div>
     </main>
   );
