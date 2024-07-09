457d7e97542a6499b96dc3630979f0dbe001ae40
nipunac99 <nipunachamod99723@gmail.com>
2024-07-09 12:39:00 +0530

    experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -17,6 +18,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
--- components/Clients.tsx
+++ components/Clients.tsx
@@ -17,12 +17,12 @@ const Clients = () => {
         />
 
         <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg: mt-10">
-            {companies.map(({id, img, name, nameImg}) => (
-                <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
-                    <img src={img} alt="{name}" className="md:w-10 w-5" />
-                    <img src={nameImg} alt="{name}" className="md:w-24 w-20" />
-                </div>
-            ))}
+          {companies.map(({ id, img, name, nameImg }) => (
+            <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+              <img src={img} alt="{name}" className="md:w-10 w-5" />
+              <img src={nameImg} alt="{name}" className="md:w-24 w-20" />
+            </div>
+          ))}
         </div>
       </div>
     </div>
