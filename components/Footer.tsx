2411be37ad2d94576aebabbdfcbbaf048cce4857
nipunac99 <nipunachamod99723@gmail.com>
2024-07-10 13:50:05 +0530

    FOOTER-FINISH

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,14 +5,10 @@ import { socialMedia } from "@/data";
 import { profile } from "console";
 
 const Footer = () => {
-  return <footer className="w-full pt-20 pb-10" id="contact">
-    <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img src="/footer-grid.svg" alt="grid" className="w-full h-full opacity-50" />
-    </div>
-
+  return <footer className="w-full pb-10 mb-[100px] md:mb-5" id="contact">
     <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-            Ready to take <span> your</span> digital presence to the next level?
+            Ready to take <span className="text-purple"> your</span> digital presence to the next level?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">Reach out to me today and let's discuss how I can help you achieve your goals.</p>
         <a href="mailto:nipunachamod99@gmail.com">
@@ -29,8 +25,10 @@ const Footer = () => {
         </p>
         <div className="flex items-center md:gap-3 gap-6">
             {socialMedia.map((profile) => (
-                <div key={profile.id}>
+                <div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 
+                bg-black-200 rounded-lg border border-black-300">
                     <img src={profile.img} alt={profile.id} 
+                    widht={20} height={20}
                     />
                 </div>
             ))}
--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid>
-        {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
+      <BentoGrid className="w-full py-20">
+        {gridItems.map((item, i) => (
           <BentoGridItem
-            id={id}
-            key={id}
-            title={title}
-            description={description}
-            className={className}
-            img={img}
-            imgClassName={imgClassName}
-            titleClassName={titleClassName}
-            spareImg={spareImg}
+            id={item.id}
+            key={i}
+            title={item.title}
+            description={item.description}
+            // remove icon prop
+            // remove original classname condition
+            className={item.className}
+            img={item.img}
+            imgClassName={item.imgClassName}
+            titleClassName={item.titleClassName}
+            spareImg={item.spareImg}
           />
         ))}
       </BentoGrid>
