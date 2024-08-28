9eeb9cb457653e571be05a45a879640aed36d598
nipunac99 <nipunachamod99723@gmail.com>
2024-08-28 16:44:50 +0530

    build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,8 +27,8 @@ const Footer = () => {
             {socialMedia.map((profile) => (
                 <div key={profile.id} className="w-10 h-10 sm:mt-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 
                 bg-black-200 rounded-lg border border-black-300">
-                    <img src={profile.img} alt={profile.id} 
-                    widht={20} height={20}
+                    <img src={profile.img} alt={'jhjhkjhj'} 
+                    width={20} height={20}
                     />
                 </div>
             ))}
