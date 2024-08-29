d6c2c323afbc3e8c6b172ea3bfcd5f78bb79d0ad
nipunac99 <nipunachamod99723@gmail.com>
2024-08-29 13:46:33 +0530

    add_projects_links_from_github

--- data/index.ts
+++ data/index.ts
@@ -76,7 +76,7 @@ export const projects = [
         des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
         img: "/p1.svg",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-        link: "https://github.com/adrianhajdin?tab=repositories",
+        link: "https://github.com/nipunac99/airy-master.git",
     },
     {
         id: 2,
@@ -84,7 +84,7 @@ export const projects = [
         des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
         img: "/p2.svg",
         iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/zoom-clone",
+        link: "https://github.com/nipunac99/patient_management_system.git",
     },
     {
         id: 3,
@@ -92,7 +92,7 @@ export const projects = [
         des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
         img: "/p3.svg",
         iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-        link: "https://github.com/adrianhajdin/ai_saas_app",
+        link: "https://github.com/nipunac99/Smart_World-ReactJs_Real_World_Project.git",
     },
     {
         id: 4,
@@ -100,7 +100,7 @@ export const projects = [
         des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
         img: "/p4.svg",
         iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-        link: "https://github.com/adrianhajdin/iphone",
+        link: "https://github.com/nipunac99/Blood_Banking_Management_System.git",
     },
 ];
 
